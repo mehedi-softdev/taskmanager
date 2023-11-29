6f733285d01383b5c027c8a5cb832ad4a11378f2
mehedi-softdev <mehedi.softdev@gmail.com>
2023-11-29 21:09:53 +0600

    Display fragment ui and base setup done

--- app/src/main/java/com/innovatenestlabs/taskmanager/DisplayFragment.kt
+++ app/src/main/java/com/innovatenestlabs/taskmanager/DisplayFragment.kt
@@ -6,11 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import com.innovatenestlabs.taskmanager.adapters.TaskListAdapter
 import com.innovatenestlabs.taskmanager.databinding.FragmentDisplayBinding
 
 class DisplayFragment : Fragment() {
     private var _binding: FragmentDisplayBinding? = null
     private val binding get() = _binding!!
+    private lateinit var taskListAdapter: TaskListAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,6 +31,9 @@ class DisplayFragment : Fragment() {
     private fun setupRecyclerView() {
         binding.rvTaskList.layoutManager = LinearLayoutManager(requireContext())
         binding.rvTaskList.setHasFixedSize(true)
+        taskListAdapter = TaskListAdapter()
+        // now set the adapter
+        binding.rvTaskList.adapter = taskListAdapter
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/innovatenestlabs/taskmanager/adapters/TaskListAdapter.kt
+++ app/src/main/java/com/innovatenestlabs/taskmanager/adapters/TaskListAdapter.kt
@@ -10,28 +10,37 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.innovatenestlabs.taskmanager.databinding.TaskListItemLayoutBinding
 import com.innovatenestlabs.taskmanager.models.Task
 
-class TaskListAdapter: ListAdapter<Task, TaskListAdapter.TaskListViewHolder>(TaskListDiffUtilCallBack()) {
+class TaskListAdapter :
+    ListAdapter<Task, TaskListAdapter.TaskListViewHolder>(TaskListDiffUtilCallBack()) {
 
-    inner class TaskListViewHolder(private val binding: TaskListItemLayoutBinding): ViewHolder(binding.root) {
+    inner class TaskListViewHolder(private val binding: TaskListItemLayoutBinding) :
+        ViewHolder(binding.root) {
+        private val taskStatusContainer =
+            mapOf<Boolean, String>(true to "Status: Completed", false to "Status: Incomplete")
+
         fun bind(task: Task) {
-
+            binding.taskName.text = task.name
+            binding.taskDueDate.text = task.date.toString()
+            // if true then take true status else false status i.e Incomplete
+            binding.taskStatus.text = taskStatusContainer[task.isComplete]
+            binding.cbIsComplete.isChecked = task.isComplete // if true then checked else unchecked
         }
     }
 
-    class TaskListDiffUtilCallBack: DiffUtil.ItemCallback<Task>() {
+    class TaskListDiffUtilCallBack : DiffUtil.ItemCallback<Task>() {
         override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
-           return oldItem.id == newItem.id
+            return oldItem.id == newItem.id
         }
 
         override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
-          return oldItem == newItem
+            return oldItem == newItem
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskListViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding =TaskListItemLayoutBinding.inflate(inflater, parent, false)
+        val binding = TaskListItemLayoutBinding.inflate(inflater, parent, false)
         return TaskListViewHolder(binding)
     }
 
